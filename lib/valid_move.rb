88319ea381bd72e7cef10672fb2a6396e1e86da8
Tristan Brown <tgbrown@stanford.edu>
2018-07-31 23:31:02 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,9 +1,9 @@
 
 def valid_move?(board,index)
-  if index.between?(0,8) && not_taken?
+  if index.between?(0,8) && position_taken?
     return true
   else
-    return false 
+    return false
 end
 end
 
